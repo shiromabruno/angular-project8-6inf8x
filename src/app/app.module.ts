3d79bcc0e532407f312b7e00f2f54d4b06cb07be
shiromabruno <bruno_shiroma@hotmail.com>
2019-08-30 21:11:20 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
-import { BagulhosComponent } from './bagulhos/bagulhos.component';
+
 
 @NgModule({
   imports: [
